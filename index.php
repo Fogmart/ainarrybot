34524c12bafed739731f01ea3b167138dfe68e2b
aevdokimov <aevdokimov@itqua.ru>
2019-06-29 15:16:50 +1000

    34

--- index.php
+++ index.php
@@ -1,11 +1,3 @@
 <?php
-/**
- * revcom_bot
- *
- * @author - Александр Штокман
- */
-header('Content-Type: text/html; charset=utf-8');
-// подрубаем API
-require_once "vendor/autoload.php";
 echo "123";
 exit;
